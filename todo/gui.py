65b68bd3a797f5e6d505166033a1f3c53d41ab83
Kyzer <kyzereye@gmail.com>
2025-08-14 10:33:19 -0600

    added the added Button

--- todo/gui.py
+++ todo/gui.py
@@ -2,14 +2,40 @@ import functions
 import FreeSimpleGUI as FSG
 
 label = FSG.Text('Type in a todo')
-input_box = FSG.InputText(tooltip = "  Enter Todo  ")
+input_box = FSG.InputText(tooltip = "  Enter Todo  ", key="todo")
 add_button = FSG.Button("Add")
 complete_button = FSG.Button('Complete')
 edit_button = FSG.Button('Edit')
+exit_button = FSG.Button('Exit')
 
 
-window = FSG.Window('My ToDo App', layout=[[label], [input_box, add_button], ])
-window.read()
+window = FSG.Window('My ToDo App', 
+                    layout=[[label], [input_box, add_button], [exit_button]], 
+                    font=('Helvetica', 20))
+
+
+while True:
+  event, values = window.read()
+
+
+
+  match event:
+    case "Add":
+      todos = functions.get_todos()
+      new_todo = values['todo']
+      todos.append(new_todo)
+      print(todos)
+      functions.write_todos(todos)
+      
+    case "Edit":
+      print('gpt edit')
+    
+    case "Exit":
+      break
+    
+    case FSG.WIN_CLOSED:
+      break
+      
 window.close()
 
 
