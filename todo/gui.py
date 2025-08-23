b307bc6681a694e09b333fd4f1a08e498b590d37
Kyzer <kyzereye@gmail.com>
2025-08-23 09:07:34 -0600

    add exit and complet function

--- todo/gui.py
+++ todo/gui.py
@@ -7,17 +7,25 @@ add_button = FSG.Button("Add")
 complete_button = FSG.Button('Complete')
 edit_button = FSG.Button('Edit')
 exit_button = FSG.Button('Exit')
+completed_button = FSG.Button('Completed')
+list_box = FSG.Listbox(values=functions.get_todos(), key='todos', 
+                       enable_events=True, size=[45,10])
 
 
 window = FSG.Window('My ToDo App', 
-                    layout=[[label], [input_box, add_button], [exit_button]], 
+                    layout=[[label], 
+                            [input_box, add_button], 
+                            [list_box, edit_button, completed_button], 
+                            [exit_button]], 
                     font=('Helvetica', 20))
 
 
 while True:
   event, values = window.read()
-
-
+  # print(1, event)
+  # print(2, values)
+  # print(3, values['todos'])
+  # print('====')
 
   match event:
     case "Add":
@@ -26,9 +34,28 @@ while True:
       todos.append(new_todo)
       print(todos)
       functions.write_todos(todos)
+      window['todos'].update(values=todos)
+
       
     case "Edit":
-      print('gpt edit')
+      todo_to_edit = values['todos'][0]
+      edited_todo = values['todo']
+      todos = functions.get_todos()
+      index_to_edit = todos.index(todo_to_edit)
+      todos[index_to_edit] = edited_todo
+      functions.write_todos(todos)
+      window['todos'].update(values=todos)
+
+    case "Completed":
+      todo_to_complete = values['todos'][0]
+      todos = functions.get_todos()
+      todos.remove(todo_to_complete)
+      functions.write_todos(todos)
+      window['todos'].update(values=todos)
+
+    case 'todos':
+      window['todo'].update(value=values['todos'][0])
+
     
     case "Exit":
       break
